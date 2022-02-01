533948f94d385a8e691738a80faba1c5066c0a44
tilak-badaya <98752348+tilak-badaya@users.noreply.github.com>
2022-02-02 01:03:47 +0530

    Add files via upload
    Loops and Iterations - For/While Loops.

---
+++ Loops and Iterations.py
@@ -0,0 +1,38 @@
+nums = [1,2,3,4,5]
+
+for num in nums:
+    print(num)
+    
+for num in nums:
+    if num ==3:
+        print('found!')
+        break
+    print(num)
+    
+for num in nums:
+    if num ==3:
+        print('found!')
+        continue
+    print(num)
+    
+for num in nums:
+    for letter in 'abc':
+        print(num,letter)
+        
+for i in range(2,10):
+    print(i)
+    
+x=0
+
+while x<10:
+    print(x)
+    if x==5:
+        break
+    x=x+1
+
+while True:
+    if x==5:
+        break
+    print(x)
+    x +=1
+    
