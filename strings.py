f10dd9c4ec7c7c562daad0533c147d077341f21c
tilak-badaya <98752348+tilak-badaya@users.noreply.github.com>
2022-02-01 19:37:00 +0530

    Add files via upload
    Strings - Working with Textual Data.

---
+++ strings.py
@@ -0,0 +1,14 @@
+greeting = 'Hello'
+name = 'john'
+
+message = greeting +" "+ name +"!"
+
+print(message)
+print(type(message))
+print(len(message))
+print(message[0])
+print(message[6:11])
+print(message[-1:5])
+print(message.upper())
+print(message.find('Hello'))
+print(message.replace('Hello','goodbye'))
