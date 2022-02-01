a1d69ebf5178cb7f13e9d83c337daaddd3221937
tilak-badaya <98752348+tilak-badaya@users.noreply.github.com>
2022-02-01 19:48:52 +0530

    Add files via upload
    Integers and Floats - Working with Numeric Data

---
+++ Integers and Floats.py
@@ -0,0 +1,32 @@
+num = 3.14
+
+print(type(num))
+print(3+2)
+print(3/2)
+print(3*2)
+print(3-1)
+print(3//2)
+print(3**2)
+print(3%2)
+print(3*(2+1))
+
+num = num+1
+
+print(num)
+
+num*=10
+
+print(num)
+print(abs(-3))
+print(round(num))
+print(3==3)
+print(3!=2)
+print(4<3)
+
+
+num_1 = '100'
+num_2 = '200'
+
+num_3 = int(num_1)+int(num_2)
+
+print(num_3)
