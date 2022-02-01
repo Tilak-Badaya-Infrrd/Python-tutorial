1e0da4af993789a6333e188113e402f7bc3aafb3
tilak-badaya <98752348+tilak-badaya@users.noreply.github.com>
2022-02-02 02:43:00 +0530

    Add files via upload
    Slicing Lists and Strings.

---
+++ Slicing.py
@@ -0,0 +1,18 @@
+my_list = [0,1,2,3,4,5,6,7,8,9]
+
+
+print(my_list[5])
+print(my_list[-1])
+print(my_list[-10])
+print(my_list[0:5])
+print(my_list[0::2])
+print(my_list[-1:1:-3])
+
+
+sample_url = 'http://facebook.com'
+
+print(sample_url)
+print(sample_url[-4:])
+print(sample_url[5:])
+print(sample_url[7:])
+print(sample_url[7:-4])
