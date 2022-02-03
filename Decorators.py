6ef06ef4f453a33d01945c80e1a79a137f8bb918
tilak-badaya <98752348+tilak-badaya@users.noreply.github.com>
2022-02-03 14:38:59 +0530

    Add files via upload
    Decorators - Dynamically Alter The Functionality Of Your Functions.

---
+++ Decorators.py
@@ -0,0 +1,66 @@
+# Decorators
+
+def outer_function(msg):
+    # message = msg
+    
+    def inner_function():
+        print(msg)
+    return inner_function
+
+hello_func = outer_function('Hello')
+bye_func = outer_function('Bye')
+hello_func()
+bye_func()
+
+
+def decorator_function(original_function):
+    def wrapper_function():
+        return original_function()
+    return wrapper_function
+
+def display():
+    print('display function ran')
+    
+decorated_display = decorator_function(display)
+
+decorated_display()
+
+from functools import wraps
+
+
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+def my_timer(orig_func):
+    import time
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+        return result
+
+    return wrapper
+
+import time
+
+
+@my_logger
+@my_timer
+def display_info(name, age):
+    time.sleep(1)
+    print('display_info ran with arguments ({}, {})'.format(name, age))
+
+display_info('Tom', 22)
